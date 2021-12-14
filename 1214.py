c1fcd0823e996a25e3183db67eee1d41e35af7a8
yeongseoni <91267819+yeongseoni@users.noreply.github.com>
2021-12-14 19:57:46 +0900

    Update 1214.py

--- 1214.py
+++ 1214.py
@@ -37,4 +37,49 @@ def solution(number, k):
 #해당 문제 풀이 위해 스택 이용함
 #스택이란? 먼저 들어온 데이터가 나중에 나가는 형식, 파이썬에서는 리스트로(append, pop)구현
 #입구와 출구가 동일한 형태
-print('hello')
+stack=[]
+stack.append(1)
+stack.append(2)
+stack.append(5)
+stack.append(6)
+stack.pop()
+stack.append(7)
+print(stack) #최하단 원소부터 출력
+print(stack[::-1]) #최상단 원소부터 출력
+
+#큐란? 먼저 들어온 데이터가 먼저 나가는 형식(선입선출), 입구와 출구가 모두 뚫려있는 터널과 같은 형태
+from collections import deque
+
+queue = deque()
+
+queue.append(5)
+queue.append(2)
+queue.append(3)
+queue.append(7)
+queue.popleft()
+queue.append(1)
+queue.append(4)
+queue.popleft()
+
+print(queue) #먼저 들어온 순서대로 출력
+queue.reverse()
+print(queue) #나중에 들어온 순서대로 출력
+print("="*100)
+print()
+
+#프로그래머스 큰 수 만들기 남의 풀이
+#문제의 정의가 혼동이 있는듯, 주어진 문자열 기본으로 해서 제일 큰 수를 만든다고 생각하면 될듯
+#k=pop한 숫자다
+def solution(number, k):
+    stack=[number[0]]
+    for num in number[1:]:
+        print('num:',num,k,stack)
+        while len(stack)>0 and stack[-1] < num and k>0:
+            k-=1
+            stack.pop()
+        print('num, k, stack:',num,k, stack)
+        stack.append(num)
+    if k!=0: #왜 해주는 걸까? 
+        stack = stack[:-k]
+    return ''.join(stack)
+solution('1231234',3)
