592421df346ae5c04279295969b6e500fb076c96
yeongseoni <91267819+yeongseoni@users.noreply.github.com>
2021-12-14 16:50:46 +0900

    1214

---
+++ 1214.py
@@ -0,0 +1,22 @@
+#프로그래머스 체육복 문제 내 풀이
+def solution(n, lost, reserve):
+    std = [0 for i in range(n) if str(i+1) in lost -1]
+    
+    for i in lost:
+        std[i-1] -=1
+    for i in reserve:
+        std[i-1] +=1
+        
+    for i in range(n):
+        if std[i] <0 and std[i+1]>=1:
+            std[i] +=1
+            std[i+1]-=1
+    cnt = 0
+    for i in std:
+        if i >=0:
+            cnt+=1
+    return cnt
+
+#프로그래머스 체육복 문제 남의 풀이
+for size in [0,1,-2]:
+    print(size)
