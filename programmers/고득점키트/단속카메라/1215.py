68f8e472df59383832e8725a5e40574b992743c0
yeongseoni <91267819+yeongseoni@users.noreply.github.com>
2021-12-15 16:34:15 +0900

    Update 1215.py

--- programmers/고득점키트/단속카메라/1215.py
+++ programmers/고득점키트/단속카메라/1215.py
@@ -1,3 +1,5 @@
+#단순하게 가장 먼저 나오는 수의 범위를 리스트로 구현해서 다음 나오는 쌍이 그 안에 들어가나?
+#안들어간다면 새로 만들고 해당 수의 범위를 붙여주는건데 10점 나옴
 def solution(routes):
     a = [i for i in range(routes[0][0],routes[0][1]+1)]
     
@@ -8,3 +10,16 @@ def solution(routes):
             a.extend([i for i in range(routes[i][0],routes[i][1]+1)])
             
     return cnt
+
+#남의 풀이
+def solution2(routes):
+    answer = 0
+    routes.sort(key=lambda x:x[1])
+    camera=-30001
+
+    for route in routes:
+        if camera < route[0]:
+            answer += 1
+            camera = route[1]
+    return answer
+
