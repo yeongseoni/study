a7dd5452fe4820675a26755580d6cb3cacc9c04f
yeongseoni <91267819+yeongseoni@users.noreply.github.com>
2021-12-28 01:45:11 +0900

    Update 기본입출력.py

--- baekjoon/기본입출력.py
+++ baekjoon/기본입출력.py
@@ -30,3 +30,29 @@ n = int(input())
 for i in range(1,n+1):
     a,b = map(int, input().split())
     print(f'Case #{i}: {a} + {b} = {a+b}')
+
+#1924
+
+#8393
+n = int(input())
+lists = [ i for i in range(1,n+1)]
+print(sum(lists))
+
+#2446
+#내가 한거
+n = int(input())
+
+for i in range(n):
+    print(' '*i + '*'*(9-(2*i)) + ' '*i)
+for i in range(n-2,-1,-1):
+    print(' '*i + '*'*(9-(2*i)) + ' '*i)
+
+#일반화
+n = int(input())
+
+for i in range(n):
+    print(' '*i + '*'*((n-i)*2-1))
+for i in range(n-2,-1,-1):
+    print(' '*i + '*'*((n-i)*2-1))
+
+
